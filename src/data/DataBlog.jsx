703ab6e56169b9bdefb330708cdd8971359016d9
suria babaei <suriyalifework60@gmail.com>
2023-10-05 14:53:36 +0330

    data for image and title

--- src/data/DataBlog.jsx
+++ src/data/DataBlog.jsx
@@ -1,21 +1,21 @@
 export const DataBlog = [
     {
         id:1,
-        imaga:'https://img.freepik.com/free-vector/ecological-strategy-development-natural-resources-access-ecologists-cartoon-characters_335657-3249.jpg',
+        imaga:'https://pas-wordpress-media.s3.amazonaws.com/content/uploads/2015/09/shutterstock_301366778-653x339.jpg',
         title: 'How to Choose the Best Blogging Platform',
         description: 'It is important to think long-term. You don’t want to join a platform that isn’t scalable to your future needs. You might find you will want to add more features to grow your blog and monetize it later',
 
     },
     {
         id:2,
-        imaga:'https://img.freepik.com/premium-vector/digital-marketing-modern-flat-concept-web-banner-design-woman-marketer-working-online-laptop_9209-8075.jpg',
+        imaga:'https://legacytips.com/wp-content/uploads/2020/03/writing-blog-content.png',
         title: 'How to Choose the Best Blogging Platform',
         description: 'It is important to think long-term. You don’t want to join a platform that isn’t scalable to your future needs. You might find you will want to add more features to grow your blog and monetize it later',
 
     },
     {
         id:3,
-        imaga:'https://img.freepik.com/premium-vector/company-credibility-brand-reputation-based-customer-loyalty_277904-23196.jpg',
+        imaga:'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/03/what-is-a-blog-1.webp',
         title: 'How to Choose the Best Blogging Platform',
         description: 'It is important to think long-term. You don’t want to join a platform that isn’t scalable to your future needs. You might find you will want to add more features to grow your blog and monetize it later',
 
@@ -42,3 +42,4 @@ export const DataBlog = [
 
     },
 ]
+
